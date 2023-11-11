0666f9b2e6988d2c83cf80ec228fd540525e3fb0
Khalid Alrasheed <71514689+kk-cycls@users.noreply.github.com>
2023-11-11 14:39:43 +0400

    feat: simplify interfacing with the client (#2)
    * v 0.0.2
    
    * Update README.md
    
    * feat: simplify interfacing with the client
    
    * new version

--- sarya/client.py
+++ sarya/client.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI, Request
 import uvicorn
 import httpx
 from typing import Any
+from functools import partial
+import inspect 
+import importlib 
+
+from sarya import UI
+from pydantic import BaseModel 
+
+class Response(BaseModel):
+    message: UI.Text
+    meta: dict[str, Any] | None = None
+
+class NewMessage(BaseModel):
+    messages: list[dict[str, Any]] | None = None
+    meta: dict[str, Any] | None = None
+
 
 from sarya import UI
 from pydantic import BaseModel 
@@ -45,19 +60,45 @@ class SaryaClient:
 
     
 
-    def run(self, host: str = "0.0.0.0", port: int = 8000):
-        uvicorn.run(self.app, host=host, port=port)
+    def run(self, main:str|None="main", host: str = "0.0.0.0", port: int = 8000):
+        caller_frame = inspect.currentframe().f_back
+        caller_module_info = inspect.getmodule(caller_frame)
+        if caller_module_info is not None:
+            caller_module_name = caller_module_info.__name__
+            module = importlib.import_module(caller_module_name)
+    
+            main_func = getattr(module, "main")
+            self.main_function = main_func
+            self.app.post("/main")(self.main)
+            uvicorn.run(self.app, host=host, port=port)
+        else:
+            raise Exception("Could not find main function")
+
 
 
     def _set_app(self):
         self.app = FastAPI(title=self.name, description=self.description, version=self.version)
         self.app.on_event("startup")(self._startup)
         self.app.get("/health_check")(self._check)
-        # self.app.on_event("shutdown")(self._shutdown)
+        self.app.on_event("shutdown")(self._shutdown)
     
-    def main(self, func):
+    def main(self, payload:NewMessage):
         # add func to be post route
-        self.app.post("/main")(func)
+        if (params:=len(inspect.signature(self.main_function).parameters)) == 2:
+            print("Params 2")
+            output = self.main_function(payload.messages, payload.meta)
+        elif params == 1:
+            print("Params 1")
+            output = self.main_function(payload.messages)
+        else:
+            print("No params")
+            output = self.main_function()
+        if isinstance(output, Response):
+            return output
+        elif isinstance(output, UI.Text):
+            output = Response(message=output)
+        return Response(**output)
+
     
     async def _startup(self):
         async with httpx.AsyncClient() as client:
@@ -68,10 +109,11 @@ class SaryaClient:
         print("Sarya is running...")
 
     
-    # async def _shutdown(self):
-    #     async with httpx.AsyncClient() as client:
-    #         await client.post(SaryaClient.sarya_url + "/sdk/marid/off", json={"name": self.name, "description": self.description, "url": self.url})
-    #     print("Sarya is shutting down...")
+
+    async def _shutdown(self):
+        async with httpx.AsyncClient() as client:
+            await client.post(SaryaClient.sarya_url + "/sdk/marid/off", json={"name": self.name, "description": self.description, "url": self.url})
+        print("Sarya is shutting down...")
 
     def _check(self):
         return {"status": "ok"}
