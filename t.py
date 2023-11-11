fd12428d2b1e73234323f9cc5023602355af6d6f
ikhalid-alrasheed <khalid@cycls.io>
2023-11-11 14:32:47 +0400

    feat: simplify interfacing with the client

--- t.py
+++ t.py
@@ -1,25 +1,6 @@
-from sarya import SaryaClient, UI, SaryaResponse
-from fastapi import Request
-
-
-if __name__ == "__main__":
-    SaryaClient.token = "test-token"
-    
-    sarya = SaryaClient(name="Test", description="Test", version="0.0.1", url="http://0.0.0.0:8001")
-
-    # @sarya.on_startup
-    # async def startup():
-
-
-    @sarya.main
-    async def main(request:Request):
-        body = await request.body()
-        print(body)
-        # history that are related to you 
-        # a way to add more history items 
-        # you do your thing
-        return SaryaResponse(message = UI.Text("Hello World"))
-    
-
-
-    sarya.run(port=8001)
+from sarya import SaryaClient, UI
+SaryaClient.token = "test-token"
+sarya = SaryaClient(name="Test", description="Test", url="http://0.0.0.0:8001")
+def main():
+    return UI.Text("Hello World")
+sarya.run(port=8001)
