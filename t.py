c9e88f61dfaccc3e0942678b66ed0a5a3d14c0ec
Khalid Alrasheed <71514689+kk-cycls@users.noreply.github.com>
2023-11-19 13:18:23 +0400

    version 0.0.2.6 (#9)
    * v 0.0.2
    
    * Update README.md
    
    * feat: simplify interfacing with the client
    
    * new version
    
    * integrat new sarya system

--- t.py
+++ t.py
@@ -1,7 +1,7 @@
 from sarya import SaryaClient, UI
-SaryaClient.token = "test-token"
-sarya = SaryaClient(name="Test", description="Test", url="http://0.0.0.0:8001")
+SaryaClient.token = "<JWT Token>"
+sarya = SaryaClient("test", name="test", description="Test")
 def main():
-    return UI.Text("Hello World")
+    return [UI.Text("Hello World"), UI.Image("https://i.imgur.com/3QXm2oK.jpeg")]
 sarya.run(port=8001)
 
